e2daef44fdaf43f77f897423422f7043097590f6
Jen Tracy <jenniferetracy@gmail.com>
2020-11-18 15:08:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,9 @@ def get_english_meaning (path, emoticon)
 end
 
 def get_japanese_emoticon(path,emoticon)
-  load_library(path)
+  load_library(path).each do |key, value|
+    if value[:english]== emoticon
+      return value[:japanese]
+  end
   
 end
