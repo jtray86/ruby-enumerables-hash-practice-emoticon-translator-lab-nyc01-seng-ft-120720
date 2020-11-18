025178924b9d5d589c6cf5b4d9d59ea2e5620076
Jen Tracy <jenniferetracy@gmail.com>
2020-11-18 14:59:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 def get_english_meaning (path, emoticon)
   load_library(path).each do |key, value|
     binding.pry
-  final_hash[key][japanese][value] = final_hash[key]
+  final_hash[key][value[1]] = final_hash[key]
 end
 end
 
